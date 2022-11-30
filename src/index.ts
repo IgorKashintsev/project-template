1596a03bbba18a703bde2bfef18385776531d1c7
IgorKashintsev <igorchello99@yandex.ru>
2022-11-30 12:53:56 +0300

    Les_2_ver_1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { renderSearchFormBlock } from './search-form.js';
+import { renderSearchFormBlock, search } from './search-form.js';
 import { renderSearchStubBlock } from './search-results.js';
 import { renderUserBlock } from './user.js';
 import { renderToast } from './lib.js';
+import { localStorage } from './localStorage.js'
+
+// export interface SearchFormData {
+//   inDate: unknown,
+//   outDate: unknown,
+//   maxPrice: unknown,
+// }
 
 window.addEventListener('DOMContentLoaded', () => {
   const date = new Date();
@@ -13,11 +20,33 @@ window.addEventListener('DOMContentLoaded', () => {
   const lastDay = new Date(date.getFullYear(), date.getMonth() + 2, 0)
     .toISOString().slice(0, 10);
 
-  renderUserBlock('Wade Warren', '/img/avatar.png', 2)
+  // const searchData = {
+  // inDate: document.getElementById('check-in-date'),
+  // outDate: document.getElementById('check-out-date'),
+  // maxPrice: document.getElementById('max-price'),
+  // };
+
+  let userName: unknown;
+  let avatar: unknown;
+  let favAmount: unknown;
+
+  const getUserData = () => {
+    userName = localStorage.user.username;
+    avatar = localStorage.user.avatarUrl;
+  }
+  const getFavoritesAmount = () => {
+    favAmount = localStorage.favoritesAmount;
+  }
+
+  getUserData();
+  getFavoritesAmount();
+
+  renderUserBlock(userName, avatar, favAmount)
   renderSearchFormBlock(dateNow, arrivalDay, departureDay, lastDay)
   renderSearchStubBlock()
   renderToast(
       {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
       {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
-  )
+  );
+  search()
 })
---
+++ src/localStorage.ts
@@ -0,0 +1,9 @@
+
+
+export const localStorage = {
+  user: {
+    username: 'Wade Warren',
+    avatarUrl: '/img/avatar.png',
+  },
+  favoritesAmount: 2,
+}
